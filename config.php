86f4f60786644f2952c5acb66632523018249fcd
mvalenciarmz <mvalenciarmz@gmail.com>
2015-12-30 22:18:31 -0600

    code added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$title = "Nombre de la Compañía"; // El título del sitio
+$company = "tlahui.com"; //sitio de la compañia
+$store_email = "store@tlahui.com"; //email de la tienda
+$admin_email = "admin@tlahui.com"; //email para notificaciones de nuevos pagos
+$item_description = "Servicios"; // Descripción default del pago
+$item_id = "0001"; // el número a mostrar en pantalla
+$script_base = "";// path para el script
+$script_location = "thank_you.php"; // pagina de agradecimiento
+
+?>
